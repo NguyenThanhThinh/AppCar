e57c2ee8e460d91732a3049b0ba18df43511031d
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-11 00:10:33 +0700

    add business

---
+++ AppCar/AppCar.Business/Business.cs
@@ -0,0 +1,19 @@
+﻿using AppCar.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCar.Business
+{
+   public class Business
+    {
+        protected AppCarDbContext context;
+        public Business()
+        {
+            this.context = new AppCarDbContext();
+        }
+        protected AppCarDbContext Context => this.context;
+    }
+}
