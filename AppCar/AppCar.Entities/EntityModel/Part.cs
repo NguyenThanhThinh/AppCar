3fc4c7435cf559e9f3090cb759359572225e5d1f
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-10 23:56:02 +0700

    add EntityModel

---
+++ AppCar/AppCar.Entities/EntityModel/Part.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCar.Entities.EntityModel
+{
+   public class Part
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double? Price { get; set; }
+        public int Quantity { get; set; }
+        public virtual ICollection<Car> Cars { get; set; }
+        public virtual Supplier Supplier { get; set; }
+    }
+}
